e6693105bc97bbd0fb822d6d6f0ddec793582585
Coumes Quentin <qcoumes@etud.u-pem.fr>
2018-11-10 17:23:36 +0100

    Removed useless middleware

--- wimsLTI/settings.py
+++ wimsLTI/settings.py
@@ -34,14 +34,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'wimsLTI.urls'
