2ca36b22da6f145458018d033dc2e151aa3716e9
Coumes Quentin <qcoumes@etud.u-pem.fr>
2019-01-24 23:36:10 +0100

    Improving django admin panel

--- wims/admin.py
+++ wims/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from wims import models
 
-admin.site.register(models.LMS)
-admin.site.register(models.WIMS)
-admin.site.register(models.WimsClass)
-admin.site.register(models.WimsUser)
+
+@admin.register(models.LMS)
+class LMSAdmin(admin.ModelAdmin):
+    list_display = ('name', 'uuid')
+    
+@admin.register(models.WIMS)
+class WIMSAdmin(admin.ModelAdmin):
+    list_display = ('id', 'dns', 'url')
+    
+@admin.register(models.WimsClass)
+class WIMSClassAdmin(admin.ModelAdmin):
+    list_display = ('id', 'wims', 'lms', 'lms_uuid', 'wims_uuid')
+    
+@admin.register(models.WimsUser)
+class WIMSUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'lms', 'lms_uuid', 'wclass', 'quser')
--- wims/models.py
+++ wims/models.py
@@ -12,6 +12,9 @@ from django.db import models
 from wims.converters import DNSConverter
 
 
+wims_help = "See 'https://wimsapi.readthedocs.io/#configuration' for more informations"
+
+
 
 class WIMS(models.Model):
     """Represents a WIMS server.
@@ -26,18 +29,17 @@ class WIMS(models.Model):
         * ident - Identifier of the wims server.
         * passwd - Password of the wims server.
         * rclass - Identifier used for each class of this WIMS server."""
-    ident = models.CharField(max_length=2048)
-    passwd = models.CharField(max_length=2048)
-    rclass = models.CharField(max_length=2048)
-    url = models.CharField(
-            max_length=2048, unique=True, db_index=True,
-            validators=[URLValidator(['http', 'https'], message="Enter a valid URL")]
-    )
     dns = models.CharField(
-            max_length=253, unique=True, db_index=True,
-            validators=[RegexValidator(DNSConverter.regex, "Enter a valid DNS")]
+        max_length=253, unique=True, db_index=True,
+        validators=[RegexValidator("^" + DNSConverter.regex + "$", "Please enter a valid DNS")]
     )
-    
+    url = models.CharField(
+        max_length=2048, unique=True, db_index=True,
+        validators=[URLValidator(['http', 'https'], message="Please enter a valid URL")]
+    )
+    ident = models.CharField(max_length=2048, help_text=wims_help)
+    passwd = models.CharField(max_length=2048, help_text=wims_help)
+    rclass = models.CharField(max_length=2048, help_text=wims_help)
     
     class Meta:
         verbose_name_plural = "WIMS"
@@ -47,7 +49,7 @@ class WIMS(models.Model):
 class LMS(models.Model):
     """Represents a LMS."""
     uuid = models.CharField(max_length=2048, primary_key=True)
-    
+    name = models.CharField(max_length=2048, null=False)
     
     class Meta:
         verbose_name_plural = "LMS"
@@ -56,11 +58,10 @@ class LMS(models.Model):
 
 class WimsClass(models.Model):
     """Represents a class on a WIMS server."""
-    wims = models.ForeignKey(WIMS, models.CASCADE)
     lms = models.ForeignKey(LMS, models.CASCADE)
-    wims_uuid = models.CharField(max_length=256)
     lms_uuid = models.CharField(max_length=256)
-    
+    wims = models.ForeignKey(WIMS, models.CASCADE)
+    wims_uuid = models.CharField(max_length=256)
     
     class Meta:
         verbose_name_plural = "WimsClasses"
@@ -73,9 +74,8 @@ class WimsUser(models.Model):
     """Represent an user on a WIMS server."""
     lms = models.ForeignKey(LMS, models.CASCADE)
     lms_uuid = models.CharField(max_length=256)
-    quser = models.CharField(max_length=256)
     wclass = models.ForeignKey(WimsClass, models.CASCADE)
-    
+    quser = models.CharField(max_length=256)
     
     class Meta:
         verbose_name_plural = "WimsUsers"
