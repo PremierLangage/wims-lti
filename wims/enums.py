fcbf980c1ee916952a5debe592dfa120ec4b220a
Coumes Quentin <qcoumes@etud.u-pem.fr>
2019-02-02 18:59:59 +0100

    Docstring and code cleanup

--- wims/enums.py
+++ wims/enums.py
@@ -6,8 +6,11 @@
 #       - Coumes Quentin <coumes.quentin@gmail.com>
 #
 
+import logging
+from enum import Enum
 
-from enumfields import Enum
+
+logger = logging.getLogger(__name__)
 
 
 
@@ -22,77 +25,63 @@ class Role(Enum):
     STAFF = 6
     ALUMNI = 7
     PROSPECTIVESTUDENT = 8
-    GUEST = 9
-    OTHER = 10
-    ADMINISTRATOR = 11
-    OBSERVER = 12
-    NONE = 13
+    CONTENTDEVELOPPER = 9
+    GUEST = 10
+    OTHER = 11
+    ADMINISTRATOR = 12
+    OBSERVER = 13
+    NONE = 14
     
+    LABEL = {
+        "Student":            STUDENT,
+        "Faculty":            FACULTY,
+        "Member":             MEMBER,
+        "Learner":            LEARNER,
+        "Instructor":         INSTRUCTOR,
+        "Mentor":             MENTOR,
+        "Staff":              STAFF,
+        "Alumni":             ALUMNI,
+        "ProspectiveStudent": PROSPECTIVESTUDENT,
+        "ContentDeveloper":   CONTENTDEVELOPPER,
+        "Guest":              GUEST,
+        "Other":              OTHER,
+        "Administrator":      ADMINISTRATOR,
+        "Observer":           OBSERVER,
+        "None":               NONE,
+    }
     
-    class Label:
-        """Label used by the Django Administration Interface.
-        
-        Correspond to roles handles definied by LIS standard."""
-        STUDENT = "Student"
-        FACULTY = "Faculty"
-        MEMBER = "Member"
-        LEARNER = "Learner"
-        INSTRUCTOR = "Instructor"
-        MENTOR = "Mentor"
-        STAFF = "Staff"
-        ALUMNI = "Alumni"
-        PROSPECTIVESTUDENT = "Prospectivestudent"
-        GUEST = "Guest"
-        OTHER = "Other"
-        ADMINISTRATOR = "Administrator"
-        OBSERVER = "Observer"
-        NONE = "None"
-    
-    
-    class URN:
-        """URN values for institution roles as defined by the LIS standard."""
-        STUDENT = "urn:lti:instrole:ims/lis/Student"
-        FACULTY = "urn:lti:instrole:ims/lis/Faculty"
-        MEMBER = "urn:lti:instrole:ims/lis/Member"
-        LEARNER = "urn:lti:instrole:ims/lis/Learner"
-        INSTRUCTOR = "urn:lti:instrole:ims/lis/Instructor"
-        MENTOR = "urn:lti:instrole:ims/lis/Mentor"
-        STAFF = "urn:lti:instrole:ims/lis/Staff"
-        ALUMNI = "urn:lti:instrole:ims/lis/Alumni"
-        PROSPECTIVESTUDENT = "urn:lti:instrole:ims/lis/ProspectiveStudent"
-        GUEST = "urn:lti:instrole:ims/lis/Guest"
-        OTHER = "urn:lti:instrole:ims/lis/Other"
-        ADMINISTRATOR = "urn:lti:instrole:ims/lis/Administrator"
-        OBSERVER = "urn:lti:insitertrole:ims/lis/Observer"
-        NONE = "urn:lti:instrole:ims/lis/None"
-        CONTENTDEVELOPPER = "urn:lti:role:ims/lis/ContentDeveloper"
-    
-    
-    @classmethod
-    def map_label(cls):
-        """Returns a dictionnary mapping label to their corresponding enum."""
-        return {r.label: r for r in iter(Role)}
+    URN = {
+        "urn:lti:instrole:ims/lis/Student":            STUDENT,
+        "urn:lti:instrole:ims/lis/Faculty":            FACULTY,
+        "urn:lti:instrole:ims/lis/Member":             MEMBER,
+        "urn:lti:instrole:ims/lis/Learner":            LEARNER,
+        "urn:lti:instrole:ims/lis/Instructor":         INSTRUCTOR,
+        "urn:lti:instrole:ims/lis/Mentor":             MENTOR,
+        "urn:lti:instrole:ims/lis/Staff":              STAFF,
+        "urn:lti:instrole:ims/lis/Alumni":             ALUMNI,
+        "urn:lti:instrole:ims/lis/ProspectiveStudent": PROSPECTIVESTUDENT,
+        "urn:lti:instrole:ims/lis/Guest":              GUEST,
+        "urn:lti:instrole:ims/lis/Other":              OTHER,
+        "urn:lti:instrole:ims/lis/Administrator":      ADMINISTRATOR,
+        "urn:lti:insitertrole:ims/lis/Observer":       OBSERVER,
+        "urn:lti:instrole:ims/lis/None":               NONE,
+        "urn:lti:role:ims/lis/ContentDeveloper":       CONTENTDEVELOPPER,
+    }
     
     
     @classmethod
-    def map_urn(cls):
-        """Returns a dictionnary mapping URN to their corresponding enum."""
-        return {r.urn: r for r in iter(Role)}
-    
-    
-    @staticmethod
-    def parse_role_lti(role):
+    def parse_role_lti(cls, role):
         """Returns the list of Roles' enums corresponding to the LTI roles.
-
-        Parameter:
-            role - (str) The role string retrieved from the LTI request."""
-        urn, label = Role.map_urn(), Role.map_label()
-        
+    
+            Parameter:
+                role - (str) The role string retrieved from the LTI request."""
         ret = []
         for r in [r.strip().title() if ":" in r else r.strip() for r in role.split(",")]:
-            if ":" in r and r in urn:
-                ret.append(urn[r])
-            elif r in label:
-                ret.append(label[r])
+            if ":" in r and r in cls.URN:
+                ret.append(cls.URN[r])
+            elif r in cls.LABEL:
+                ret.append(cls.LABEL[r])
+            else:
+                logger.warning("Received unknown LTI role: '%s'" % r)
+        
         return ret
-
--- wims/exceptions.py
+++ wims/exceptions.py
@@ -6,6 +6,7 @@
 #       - Coumes Quentin <coumes.quentin@gmail.com>
 #
 
+
 class BadRequestException(Exception):
     """Raised if the LTI request is not valid."""
     pass
