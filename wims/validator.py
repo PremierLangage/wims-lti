41c8975dbeebc4e77a5a74e51f8ba120b4b5e9c1
Coumes Quentin <qcoumes@etud.u-pem.fr>
2019-02-03 18:25:40 +0100

    Remove useless method

--- wims/validator.py
+++ wims/validator.py
@@ -20,111 +20,23 @@ logger = logging.getLogger(__name__)
 class RequestValidator(BaseRequestValidator):
     """Implements a RequestValidator according to
     https://oauthlib.readthedocs.io/en/latest/oauth1/validator.html"""
-
+    
     client_key_length = (3, 100)
     nonce_length = (3, 100)
     
+    
     def validate_client_key(self, client_key, request):
-        b = client_key in settings.LTI_OAUTH_CREDENTIALS
-        if not b:
-            logger.info("Invalid LTI request: '%s' not in LTI_OAUTH_CREDENTIALS")
-        return b
-    
-    
-    def validate_request_token(self, client_key, token, reques):
-        return True
-    
-    
-    def validate_access_token(self, client_key, token, reques):
-        return True
+        """Check that client_key is declared in LTI_OAUTH_CREDENTIALS."""
+        return client_key in settings.LTI_OAUTH_CREDENTIALS
     
     
     def validate_timestamp_and_nonce(self, client_key, timestamp, nonce, request,
                                      request_token=None, access_token=None):
-        b = int(time.time()) - int(timestamp) < 1800
-        if not b:
-            logger.info("Invalid LTI request: timestamp outdated")
-        return b
-    
-    
-    def validate_redirect_uri(self, client_key, redirect_uri, request):
-        return True
-    
-    
-    def validate_requested_realms(self, client_key, realms, request):
-        return True
-    
-    
-    def validate_realms(self, client_key, token, request, uri=None, realms=None):
-        return True
-    
-    
-    def validate_verifier(self, client_key, token, verifier, request):
-        return True
-    
-    
-    def invalidate_request_token(self, client_key, request_token, request):
-        pass
+        """Check that timestamp is not older than 30 minutes."""
+        return int(time.time()) - int(timestamp) < 1800
     
     
     def get_client_secret(self, client_key, request):
+        """Retrieve the secret from  LTI_OAUTH_CREDENTIALS with the given client_key, 'dummy'
+        if not found."""
         return settings.LTI_OAUTH_CREDENTIALS.get(client_key, "dummy")
-    
-    
-    def get_request_token_secret(self, client_key, token, reques):
-        return "dummy"
-    
-    
-    def get_access_token_secret(self, client_key, token, request):
-        return "dummy"
-    
-    
-    def get_rsa_key(self, client_key, request):
-        return "dummy"
-    
-    
-    def get_realms(self, token, request):
-        return []
-    
-    
-    def get_default_realms(self, client_key, request):
-        return []
-    
-    
-    def get_redirect_uri(self, token, request):
-        return "dummy"
-    
-    
-    def save_request_token(self, token, request):
-        pass
-    
-    
-    def save_verifier(self, token, verifier, request):
-        pass
-    
-    
-    def save_access_token(self, token, request):
-        pass
-    
-    
-    def verify_realms(self, token, realms, request):
-        return True
-    
-    
-    def verify_request_token(self, token, request):
-        return True
-    
-    
-    @property
-    def dummy_client(self):
-        return "dummy"
-    
-    
-    @property
-    def dummy_request_token(self):
-        return "dummy"
-    
-    
-    @property
-    def dummy_access_token(self):
-        return "dummy"
