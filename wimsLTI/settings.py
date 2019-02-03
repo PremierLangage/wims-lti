e852bd43ae685db1f301865e4561cac71d4ec24b
Coumes Quentin <qcoumes@etud.u-pem.fr>
2019-02-03 15:26:00 +0100

    Added django_coverage_plugin

--- wimsLTI/settings.py
+++ wimsLTI/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
         'DIRS':     [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS':  {
+            'debug':              DEBUG,
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
@@ -70,6 +71,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
+        
     },
 ]
 
