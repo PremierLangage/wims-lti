d0697c4ac4795c181e8f14477d308e27fce176e8
Coumes Quentin <qcoumes@etud.u-pem.fr>
2019-02-03 01:28:28 +0100

    Removed csrf middleware

--- wimsLTI/settings.py
+++ wimsLTI/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
