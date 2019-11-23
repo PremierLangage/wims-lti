f3351c208305e150677a08f45df1eaabeb06c046
Coumes Quentin <qcoumes@etud.u-pem.fr>
2019-11-23 20:35:27 +0100

    #32 Added tasks to automatically send grade back to the LMS

--- lti_app/apps.py
+++ lti_app/apps.py
@@ -8,8 +8,50 @@
 
 import warnings
 
+from apscheduler.schedulers.background import BackgroundScheduler
 from django.apps import AppConfig
 from django.conf import settings
+
+from lti_app.tasks import send_back_all_exams_grades, send_back_all_sheets_grades
+
+
+
+def display_warnings():
+    """Display warning for missing settings"""
+    if settings.DEBUG:
+        warnings.warn(
+            "Settings 'DEBUG' has not been redefined in wimsLTI/config.py, "
+            "see https://docs.djangoproject.com/fr/2.2/ref/settings/#debug",
+            Warning
+        )
+    
+    if settings.SECRET_KEY == '-qj!o^8$@!&7))^77^z8(-5rp*5x=7q(736)05x$h(inkfm^1#':
+        warnings.warn(
+            "Settings 'SECRET_KEY' has not been redefined in wimsLTI/config.py, "
+            "see https://docs.djangoproject.com/fr/2.2/ref/settings/#secret-key",
+            Warning
+        )
+    
+    if settings.SERVER_EMAIL == "root@localhost":
+        warnings.warn(
+            "Settings 'SERVER_EMAIL' has not been redefined in wimsLTI/config.py, "
+            "see https://docs.djangoproject.com/fr/2.2/ref/settings/#server-email",
+            Warning
+        )
+    
+    if settings.EMAIL_HOST == "localhost":
+        warnings.warn(
+            "Settings 'EMAIL_HOST' has not been redefined in wimsLTI/config.py, "
+            "see https://docs.djangoproject.com/fr/2.2/ref/settings/#email-host",
+            Warning
+        )
+    
+    if not settings.ADMINS:
+        warnings.warn(
+            "Settings 'ADMINS' has not been redefined in wimsLTI/config.py, "
+            "see https://docs.djangoproject.com/fr/2.2/ref/settings/#admins",
+            Warning
+        )
 
 
 
@@ -18,16 +60,17 @@ class LtiAppConfig(AppConfig):
     
     
     def ready(self):
-        if settings.SERVER_EMAIL == "root@localhost":
-            warnings.warn(
-                "Settings 'SERVER_EMAIL' has not been redefined in config.py, "
-                "see https://docs.djangoproject.com/fr/2.2/ref/settings/#server-email",
-                Warning
-            )
+        """Display warning for missing settings and set up scheduled tasks."""
         
-        if settings.EMAIL_HOST == "localhost":
-            warnings.warn(
-                "Settings 'EMAIL_HOST' has not been redefined in config.py, "
-                "see https://docs.djangoproject.com/fr/2.2/ref/settings/#email-host",
-                Warning
-            )
+        display_warnings()
+        
+        scheduler = BackgroundScheduler(job_defaults={
+            'coalesce':           True,
+            'max_instances':      1,
+            'misfire_grace_time': 60 * 10,
+        })
+        scheduler.add_job(send_back_all_sheets_grades,
+                          trigger=settings.SEND_GRADE_BACK_CRON_TRIGGER)
+        scheduler.add_job(send_back_all_exams_grades,
+                          trigger=settings.SEND_GRADE_BACK_CRON_TRIGGER)
+        scheduler.start()
---
+++ lti_app/tasks.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#
+#  stasks.py
+#
+#  Authors:
+#       - Coumes Quentin <coumes.quentin@gmail.com>
+
+import logging
+
+from django.apps import apps
+
+
+logger = logging.getLogger(__name__)
+
+
+
+def send_back_all_sheets_grades():
+    """Send back the grades of every User of every WimsSheet to their corresponding LMS."""
+    GradeLinkSheet = apps.get_model("lti_app", "GradeLinkSheet")
+    WimsSheet = apps.get_model("lti_app", "WimsSheet")
+    total = 0
+    
+    print("OK sheets")
+    logger.info("Sending grades of every User of every WimsSheet to their LMS")
+    for sheet in WimsSheet.objects.all():
+        total = + GradeLinkSheet.send_back_all(sheet)
+    logger.info("Done sending grades of every User of every WimsSheet to their LMS (%d sent)"
+                % total)
+    return total
+
+
+
+def send_back_all_exams_grades():
+    """Send back the grades of every User of every WimsExam to their corresponding LMS."""
+    GradeLinkExam = apps.get_model("lti_app", "GradeLinkExam")
+    WimsExam = apps.get_model("lti_app", "WimsExam")
+    total = 0
+
+    print("OK exams")
+    logger.info("Sending grades of every User of every WimsExam to their LMS")
+    for exam in WimsExam.objects.all():
+        total = + GradeLinkExam.send_back_all(exam)
+    logger.info("Done sending grades of every User of every WimsExam to their LMS (%d sent)"
+                % total)
+    return total
